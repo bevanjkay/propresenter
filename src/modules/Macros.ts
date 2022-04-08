9fe0b72c853b99ed9bbf5326a3e16df87541f7bf
Shadow <william.shadow@outlook.com>
2022-04-08 15:52:57 -0500

    Add types for Macros#fetchAll

--- src/modules/Macros.ts
+++ src/modules/Macros.ts
@@ -5,9 +5,9 @@ import Macro from "../structures/Macro"
 import Color from "../types/color"
 
 class Macros extends BaseModule {
-    async fetchAll() {
+    async fetchAll(): Promise<Collection<string, Macro>> {
         const result = await this.client.request("/macros")
-        const macrosReturned = new Collection()
+        const macrosReturned: Collection<string, Macro> = new Collection()
         result.forEach((r: APIMacro) => {
             macrosReturned.set(r.id.name, new Macro(r))
         })
@@ -24,17 +24,29 @@ class Macros extends BaseModule {
         return new Macro(result)
     }
 
+    /**
+     * @remarks The ID you pass here can either be the UUID of the macro, the name of the macro, or the 0-based index of the macro.
+     */
+
     async trigger(id: string | number) {
         const fetchId = `${id}`
         await this.client.request(`/macro/${fetchId}/trigger`)
         return true
     }
 
+    /**
+     * @remarks The ID you pass here can either be the UUID of the macro, the name of the macro, or the 0-based index of the macro.
+     */
+
     async delete(id: string | number) {
         const fetchId = `${id}`
         await this.client.request(`/macro/${fetchId}/delete`, {}, "DELETE")
         return true
     }
+
+    /**
+     * @remarks The ID you pass here can either be the UUID of the macro, the name of the macro, or the 0-based index of the macro.
+     */
 
     async setName(id: string | number, name: string) {
         const fetchId = `${id}`
@@ -48,6 +60,10 @@ class Macros extends BaseModule {
         )
         return new Macro(newMacro.field_macro) // API docs say it should just be newMacro, but it's actually newMacro.field_macro
     }
+
+    /**
+     * @remarks The ID you pass here can either be the UUID of the macro, the name of the macro, or the 0-based index of the macro.
+     */
 
     async setColor(id: string | number, color: Color) {
         const fetchId = `${id}`
